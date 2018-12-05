32ebdd56c6c1584e48f32472d545962f78cf2407
Dave Hendler <sollaires@gmail.com>
2018-12-05 12:37:31 -0800

    Add an optimized version that actually uses a proper stack rather than just string-as-stack that runs ~100x faster

---
+++ day5/perf.js
@@ -0,0 +1,43 @@
+let fs = require('fs');
+const path = require('path');
+
+const _ = require('lodash');
+const Promise = require('bluebird');
+
+fs = Promise.promisifyAll(fs);
+
+const main = async () => {
+  const data = await fs.readFileAsync(path.join(__dirname, 'input.txt'), 'utf8');
+  const lines = _.filter(_.split(data, '\n'));
+
+  const test = 'abBA';
+
+  let range = 'abcdefghijklmnopqrstuvwxyz';
+  _.each(range, removeChar => {
+    const charRemoved = lines[0].replace(new RegExp(removeChar, 'gi'), '');
+
+    let prev;
+    let output = [];
+    _.each(charRemoved, char => {
+      if (!prev) {
+        prev = char;
+        output.push(char);
+      } else {
+        if (prev !== char && _.toUpper(prev) === _.toUpper(char)) {
+          output.pop();
+          prev = output[output.length - 1];
+        } else {
+          output.push(char);
+          prev = char;
+        }
+      }
+    });
+    console.log(output.length, removeChar);
+  });
+
+  // node day5/solve.js | sort -g
+
+  console.log('done');
+};
+
+main();
