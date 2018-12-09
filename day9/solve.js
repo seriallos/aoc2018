26584e4d9692642fa068ef96ad6f09603222938d
Dave Hendler <sollaires@gmail.com>
2018-12-08 23:41:22 -0800

    Add day9, rank 1302

---
+++ day9/solve.js
@@ -0,0 +1,119 @@
+let fs = require('fs');
+const path = require('path');
+
+const _ = require('lodash');
+const Promise = require('bluebird');
+
+fs = Promise.promisifyAll(fs);
+
+const file = process.argv[2];
+
+const IS_TEST_DATA = path.basename(file) === 'test.txt';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.previous = null
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.size = 0;
+    this.current = null;
+  }
+  addMarble(value) {
+    const node = new Node(value);
+    if (this.size === 0) {
+      node.next = node;
+      node.previous = node;
+      this.current = node;
+    } else {
+      const insert = this.current.next;
+
+      node.previous = insert;
+      node.next = insert.next;
+
+      node.previous.next = node;
+      node.next.previous = node;
+
+      this.current = node;
+    }
+    this.size++;
+  }
+  remove() {
+    let remove = this.current;
+    let move = 7;
+    while (move-- > 0) {
+      remove = remove.previous;
+    }
+
+    remove.previous.next = remove.next;
+    remove.next.previous = remove.precious;
+
+    this.current = remove.next;
+
+    this.size--;
+
+    return remove.value;
+  }
+  print() {
+    let cursor = this.current;
+    do {
+      process.stdout.write(' ' + cursor.value);
+      cursor = cursor.next;
+    } while (cursor !== this.current);
+    process.stdout.write('\n');
+  }
+}
+
+const main = async () => {
+  const data = await fs.readFileAsync(path.join(file), 'utf8');
+  const lines = _.filter(_.split(data, '\n'));
+  const first = lines[0];
+
+  if (IS_TEST_DATA) {
+    console.log('Running test data');
+  } else {
+    console.log('Running real data');
+  }
+
+  console.log();
+
+  let [, numPlayers, numMarbles] = first.match(/(\d+) .* (\d+)/);
+  numPlayers = parseInt(numPlayers, 10);
+  // remove the 100 * for part 1 solution
+  numMarbles = 100 * parseInt(numMarbles, 10);
+
+  let currentPlayer;
+  const scores = [];
+
+  for (let i = 0; i < numPlayers; i++) {
+    scores[i] = 0;
+  }
+
+  const list = new DoublyLinkedList();
+
+  for (let turn = 0; turn <= numMarbles; turn++) {
+    currentPlayer = turn % numPlayers + 1;
+
+    if (turn === 0 || turn % 23 !== 0) {
+      list.addMarble(turn);
+    } else {
+      const removed = list.remove();
+
+      scores[currentPlayer - 1] += removed + turn;
+    }
+
+    if (turn < 30) {
+      list.print();
+    }
+  }
+
+  console.log('High score', _.max(scores));
+
+  console.log('Done');
+};
+
+main();
